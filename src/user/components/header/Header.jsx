b846c0ba719011ea8c29b39f3fee306607b72ce8
PukeoLnk19 <phukeo1920@gmail.com>
2024-03-29 15:12:09 +0700

    Update

--- src/user/components/header/Header.jsx
+++ src/user/components/header/Header.jsx
@@ -6,6 +6,7 @@ import { Link, useLocation } from "react-router-dom";
 import Logo from "../../../img/Logo.png";
 import storename from "../../../img/storename.png";
 import { AiFillDashboard } from "react-icons/ai";
+import { FaYoutube } from "react-icons/fa";
 
 const Header = ({ handleSearch }) => {
   // For authenticate user
@@ -71,6 +72,11 @@ const Header = ({ handleSearch }) => {
               <div className="boxsearchContainer">
                   <Link to="/cart">
                     <FaCartShopping className="head_colorr" />
+                  </Link>
+              </div>
+              <div className="boxsearchContainer">
+                  <Link to="https://www.youtube.com/">
+                    <FaYoutube className="head_colorr" />
                   </Link>
               </div>
               <div className="icon_account_login">
